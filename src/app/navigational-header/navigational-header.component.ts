ff846ca60ca0c092ec242070064955c4558ecb48
Akash srivastava <43230321+toughguyas@users.noreply.github.com>
2023-06-07 18:22:12 +0530

    NEW Nav

--- src/app/navigational-header/navigational-header.component.ts
+++ src/app/navigational-header/navigational-header.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navigational-header',
   templateUrl: './navigational-header.component.html',
   styleUrls: ['./navigational-header.component.css']
 })
-export class NavigationalHeaderComponent {
+export class NavigationalHeaderComponent implements OnInit {
+  public navToggle = false;
+ 
+  constructor() {
 
+  }
+  public ngOnInit(): void {
+    // window.onresize = () => this.navToggle = window.innerWidth <= 1130;
+  }
 }
