4c173bf252f24ee688d0d40b2868d72fa2543a8f
Akash srivastava <43230321+toughguyas@users.noreply.github.com>
2023-06-08 21:55:40 +0530

    cover changes

--- src/app/cover/cover.component.ts
+++ src/app/cover/cover.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-cover',
   templateUrl: './cover.component.html',
   styleUrls: ['./cover.component.css']
 })
-export class CoverComponent {
+export class CoverComponent implements OnInit {
+  public work = [ 'FULL STACK DEVELOPER' , 'PHILOLOGIST', 'ENTERTAINER','LOVER']
+  public assignwork= 'FULL STACK DEVELOPER' ;
+  public j=1;
+  constructor() {
+
+  }
+  public ngOnInit(): void {
+        setInterval(()=>{
+          this.assignwork = this.work[this.j];
+          this.j=this.j+1;
+          if(this.j==4) {
+            this.j=0;
+          }
+        },2000);    
+  }
 
 }
