56e42846397bdb61f601b60117c6eb08b268fe59
Akash srivastava <43230321+toughguyas@users.noreply.github.com>
2023-06-10 12:47:41 +0530

    NAvigation changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'akash-portfolio';
+  constructor(private activatedRoute:ActivatedRoute) {
+
+  }
+  public ngOnInit(): void {
+    this.activatedRoute.fragment.subscribe(res=>{
+      console.log(res)
+      this.jumptoabout(res);
+    });  
+}
+ 
+  public jumptoabout(res:any) {
+    setTimeout(() => {
+      document.getElementById(res)?.scrollIntoView({behavior: "smooth"});
+    }, 1000);
+  }
 }
