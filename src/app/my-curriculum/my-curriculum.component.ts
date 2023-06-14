9256517bc03cabe3a68a599e259b0bcd47f92d58
Akash srivastava <43230321+toughguyas@users.noreply.github.com>
2023-06-14 21:05:35 +0530

    biodata

--- src/app/my-curriculum/my-curriculum.component.ts
+++ src/app/my-curriculum/my-curriculum.component.ts
@@ -6,5 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./my-curriculum.component.css']
 })
 export class MyCurriculumComponent {
+  public edu = false;
+  public exp = false;
+  public skills = false;
+  public award = false;
+  public goto (str:string){
+   if(str === 'Education') {
+    this.edu = true;
+    this.exp = false;
+    this.skills = false;
+    this.award = false;
 
+   } else if(str === 'Experience') {
+    this.edu = false;
+    this.exp = true;
+    this.skills = false;
+    this.award = false;
+
+   } else if(str === 'Skills') {
+    this.edu = false;
+    this.exp = false;
+    this.skills = true;
+    this.award = false;
+
+   } else if(str === 'Awards') {
+    this.edu = false;
+    this.exp = false;
+    this.skills = false;
+    this.award = true;
+
+   }
+  }
 }
